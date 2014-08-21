cc3b5fd30188fe3fb66ac10afe824a053af4d3a4
Ali Bahman <abahman@takeawayit.com>
2014-08-22 00:40:09 +0100

    Adding ocra service manager

--- config/application.config.php
+++ config/application.config.php
@@ -6,6 +6,7 @@ return array(
         'ZendDeveloperTools',
         'DoctrineModule',
         'DoctrineORMModule',
+        'OcraServiceManager'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
